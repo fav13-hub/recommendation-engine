d9ae4fbc6453f0d713f120d2dee31d17b52592cc
fav13-hub <obiomaf27@gmail.com>
2025-12-25 01:20:58 +0100

    removed the unnecessary comment.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 users = pd.read_csv("data/users.csv")
 user_ids = users["user_id"].tolist()
 
-print("🔥 HOT START SIMULATION")
+print("HOT START SIMULATION")
 hot_result = hot_start(post_id=101, user_ids=user_ids)
 print(hot_result)
 
